f7bed8742c4852bca232be396c79102b061bbe69
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2022-05-09 01:11:45 +0400

    replace stimulus with vue dependency

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    .enableVueLoader(() => {}, { runtimeCompilerBuild: false })
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
