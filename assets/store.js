e1659c1f08780ab5afb1e642e265af5a77b1d53f
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2022-05-29 16:35:41 +0400

    add vue router and pinia dependencies

---
+++ assets/store.js
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useStore = defineStore('main', {
+  state: () => ({}),
+  getters: {},
+  actions: {},
+});
