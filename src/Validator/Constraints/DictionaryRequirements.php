95d4fbe3b1f0b6fd5f7b7d1d2eff17f6369196f2
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2022-05-30 04:26:46 +0400

    change dictionary validation messages

--- src/Validator/Constraints/DictionaryRequirements.php
+++ src/Validator/Constraints/DictionaryRequirements.php
@@ -12,26 +12,53 @@ class DictionaryRequirements extends Compound
     {
         return [
             new Assert\Collection([
-                'name' => new Assert\Required([
-                    new Assert\NotNull(),
-                    new Assert\NotBlank(),
-                    new Assert\Length(['min' => 2, 'max' => 255])
-                ]),
-                'dictionary' => new Assert\Required([
-                    new Assert\NotNull(),
-                    new Assert\File([
-                        'maxSize' => '10M',
-                        'mimeTypes' => ['text/csv', 'text/plain']
-                    ])
-                ]),
-                'images' => new Assert\Optional([
-                    new Assert\NotNull(),
-                    new Assert\File([
-                        'maxSize' => '10M',
-                        'mimeTypes' => ['application/zip']
-                    ])
-                ]),
+                'fields' => $this->getFields(),
+                'missingFieldsMessage' => 'Поле обязательно для заполнения.'
             ])
+        ];
+    }
+
+    private function getFields(): array
+    {
+        return [
+            'name' => new Assert\Required([
+                new Assert\NotNull([
+                    'message' => 'Значение не должно быть null.'
+                ]),
+                new Assert\NotBlank([
+                    'message' => 'Значение не должно быть пустым.'
+                ]),
+                new Assert\Length([
+                    'min' => 2,
+                    'max' => 255,
+                    'minMessage' => 'Значение слишком короткое. Оно должно состоять из {{ limit }} символов или более.',
+                    'maxMessage' => 'Значение слишком длинное. Оно должно содержать {{ limit }} символов или меньше.'
+                ])
+            ]),
+            'dictionary' => new Assert\Required([
+                new Assert\NotNull([
+                    'message' => 'Значение не должно быть null.'
+                ]),
+                new Assert\File([
+                    'maxSize' => '10M',
+                    'mimeTypes' => ['text/csv', 'text/plain'],
+                    'maxSizeMessage' => 'Файл слишком большой ({{ size }} {{ suffix }}). Допустимый максимальный размер: {{ limit }} {{ suffix }}.',
+                    'mimeTypesMessage' => 'Файл должен быть в формате CSV.',
+                    'notFoundMessage' => 'Не удалось найти файл.'
+                ])
+            ]),
+            'images' => new Assert\Optional([
+                new Assert\NotNull([
+                    'message' => 'Значение не должно быть null.'
+                ]),
+                new Assert\File([
+                    'maxSize' => '10M',
+                    'mimeTypes' => ['application/zip'],
+                    'maxSizeMessage' => 'Файл слишком большой ({{ size }} {{ suffix }}). Допустимый максимальный размер: {{ limit }} {{ suffix }}.',
+                    'mimeTypesMessage' => 'Файл должен быть в формате ZIP.',
+                    'notFoundMessage' => 'Не удалось найти файл.',
+                ])
+            ]),
         ];
     }
 }
