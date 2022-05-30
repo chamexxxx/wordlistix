02a88b8a5516478a03d682dd84bfc70833f214d4
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2022-05-30 04:54:03 +0400

    create dictionary list component

---
+++ assets/components/DictionaryList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="pr-2 overflow-y-auto scroll">
+    <RouterLink
+        v-for="({ id, name }, index) in items"
+        :to="{ name: 'Dictionary', params: { id } }"
+        :key="index"
+        class="block w-full mb-2 last:mb-0 px-5 py-3 bg-white rounded-md cursor-pointer
+                hover:bg-gray-100 active:bg-gray-200"
+    >
+      <h3 class="font-medium leading-5">
+        {{ name }}
+      </h3>
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
