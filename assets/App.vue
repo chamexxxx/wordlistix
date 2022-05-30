9e5bac0bce982346cd085758f1829ecdba7f8cdc
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2022-05-30 04:47:51 +0400

    create app layout

--- assets/App.vue
+++ assets/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div></div>
+  <div class="min-h-screen flex flex-col items-center px-5 pt-24 pb-10
+    bg-gradient-to-r from-cyan-400 to-sky-500">
+    <h1 class="mb-10 text-5xl text-white text-center">
+      wordlistix
+    </h1>
+
+    <NavTabs :links="routerLinks" />
+
+    <RouterView />
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-};
+<script setup>
+import { onMounted } from 'vue';
+import { useStore } from './store';
+import NavTabs from './components/base/NavTabs.vue';
+
+const routerLinks = [];
 </script>
