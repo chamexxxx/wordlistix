3515af777c593bab6bfe7d06a3e636ea575f5bc4
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2022-05-30 04:49:25 +0400

    create comparison list component

---
+++ assets/components/ComparisonList.vue
@@ -0,0 +1,30 @@
+<template>
+  <RecycleScroller
+    :items="items"
+    :item-size="145"
+    key-field="id"
+    class="scroll"
+    v-slot="{ item }"
+  >
+    <div class="pb-3">
+      <ComparisonListItem
+          :words="item.words"
+          :image="item.image"
+          :dictionary-name="item.dictionary.name"
+      />
+    </div>
+  </RecycleScroller>
+</template>
+
+<script setup>
+import { RecycleScroller } from 'vue-virtual-scroller';
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
+import ComparisonListItem from './ComparisonListItem.vue';
+
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
---
+++ assets/components/ComparisonListItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex justify-between px-5 py-4 bg-gray-100 rounded-md">
+    <div>
+      <p
+          v-for="({ languageCode, text }, index) in words"
+          :key="index"
+          class="leading-8"
+      >
+        {{ text }}
+
+        <span class="ml-1 px-1 py-0.5 text-xs text-blue-600 bg-blue-200 rounded-sm">
+          {{ languageCode }}
+        </span>
+      </p>
+
+      <hr class="my-2">
+
+      <p class="text-sm text-gray-500">
+        {{ dictionaryName }}
+      </p>
+    </div>
+
+    <img v-if="image"
+         :src="`/uploads/${image}`"
+         class="w-20 h-20 object-cover rounded-full"
+         alt=""
+    >
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  words: {
+    type: Array,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  dictionaryName: {
+    type: String,
+    required: true,
+  },
+});
+</script>
