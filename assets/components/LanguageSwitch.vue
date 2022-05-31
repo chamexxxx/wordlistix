43672c3773370d2694f91160a7c696a72ef48a50
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2022-05-31 07:32:04 +0400

    fix language switch

--- assets/components/LanguageSwitch.vue
+++ assets/components/LanguageSwitch.vue
@@ -5,21 +5,21 @@
       class="relative inline-flex items-center justify-between gap-x-1.5 h-6 px-1.5 rounded-full
         transition duration-300 ease-in-out"
   >
-    <span v-if="!enabled" class="ml-1 text-white">
+    <span v-if="enabled" class="ml-1 text-white">
       {{ languageCodes[0] }}
     </span>
 
     <span class="inline-block h-4 w-4 rounded-full bg-white" />
 
-    <span v-if="enabled" class="mr-1 text-white">
+    <span v-if="!enabled" class="mr-1 text-white">
       {{ languageCodes[1] }}
     </span>
   </Switch>
 </template>
 
 <script setup>
-import { Switch } from '@headlessui/vue';
 import { ref, watch } from 'vue';
+import { Switch } from '@headlessui/vue';
 
 const props = defineProps({
   modelValue: String,
@@ -32,13 +32,13 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue']);
 
-const enabled = ref(false);
+const enabled = ref(null);
 
 watch(() => props.modelValue, (value) => {
-  enabled.value = value !== props.languageCodes[0];
-});
+  enabled.value = value === props.languageCodes[0];
+}, { immediate: true });
 
 watch(enabled, (value) => {
-  emit('update:modelValue', !value ? props.languageCodes[0] : props.languageCodes[1]);
+  emit('update:modelValue', props.languageCodes[value ? 0 : 1]);
 });
 </script>
